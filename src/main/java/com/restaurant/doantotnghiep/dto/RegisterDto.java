7fb7801bbf477575caafed814d1d543ad1bece5f
NhanThan271 <nhantran.13082005@gmail.com>
2026-04-16 12:31:03 +0700

    feat: sua lai chuc nang dang ky

--- src/main/java/com/restaurant/doantotnghiep/dto/RegisterDto.java
+++ src/main/java/com/restaurant/doantotnghiep/dto/RegisterDto.java
@@ -1,59 +1,16 @@
 package com.restaurant.doantotnghiep.dto;
 
+import lombok.*;
+
+@Getter
+@Setter
 public class RegisterDto {
     private String username;
+    private String fullName;
     private Long branchId;
     private String email;
+    private String phone;
     private String password;
     private String role;
     private String position;
-
-    // getters & setters
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Long getBranchId() {
-        return branchId;
-    }
-
-    public void setBranchId(Long branchId) {
-        this.branchId = branchId;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
 }
